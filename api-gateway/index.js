315c7a2833d9f72e51bef0e3a6fba7c3a55afafd
Geet Trivedi <geet.exe@gmail.com>
2026-06-15 00:57:04 +0530

    feat: added rate limiter

--- api-gateway/index.js
+++ api-gateway/index.js
@@ -3,13 +3,24 @@ const morgan = require('morgan');
 const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
+const rateLimit = require('express-rate-limit');
 const apiGatewayRouter = require('./src/routes/apiGateway.router');
 const app = express();
+
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, //15 mins
+    max: 100, // max 100 req per IP per window
+    message: {
+        success: false,
+        message: 'Too many requests, please try again later.'
+    }
+});
 
 // app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
 app.use(morgan('dev'));
+app.use(limiter);
 
 const PORT = process.env.PORT || 3000;
 
